4a50a483da027f08f357967b309c6c24f5ee15e6
Akshaynanda96 <161761333+Akshaynanda96@users.noreply.github.com>
2024-06-02 20:27:21 +0000

    datatype converstion

---
+++ 01_bacis/01_converstion.js
@@ -0,0 +1,27 @@
+// console.log (33)
+
+let student = 33
+// console.log(typeof(student));
+
+let name = 'akshay'
+// console.log(typeof(name));
+
+let rollnumber = 33
+let strrollnum = String(rollnumber)
+console.log(typeof(strrollnum));
+let boolrollnum = Boolean(rollnumber)
+console.log(typeof(boolrollnum));
+let Integerrollnum = Number(rollnumber)
+console.log(typeof(Integerrollnum));
+let objrollnum = Object(rollnumber)
+console.log(typeof(objrollnum));
+
+// this the type datatype cast
+// if you want to see type of any value use typeof
+// if check in booline value 1 is true and 0  is false
+// this is one error in js if your string is alfnumeric and you convert into intiger is convert and 
+// print value is NaN
+// typeof string
+// typeof Number
+// typeof Booline 
+// typeof object
