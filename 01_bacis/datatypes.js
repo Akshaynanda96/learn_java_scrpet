3e69a8ead2a14d178f4577aa1748ea03085b7e11
Akshaynanda96 <161761333+Akshaynanda96@users.noreply.github.com>
2024-06-03 18:16:59 +0000

    data_types

---
+++ 01_bacis/datatypes.js
@@ -0,0 +1,29 @@
+// how many data types in java
+
+// primitiv and NONprmitive
+
+// primitiv
+// 7 types of primitiv 
+
+strinig , Number , booline , symbool , bigint, undifined , None
+
+// Nonprimitiv
+// 3 types
+
+object , funtion , Array
+
+
+
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
